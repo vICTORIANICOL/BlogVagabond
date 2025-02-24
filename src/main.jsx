9dd519843470c4a3767fd7f71d16f1d707a9ddd8
Victoria <nicol_victoria@yahoo.com>
2025-02-24 18:21:17 +0100

    +6

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import {  HashRouter } from "react-router-dom"; // Import BrowserRouter
 import "./index.css";
 import App from "./App.jsx";
 
+// HASHROUTER
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
